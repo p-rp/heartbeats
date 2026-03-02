1c3fa9d5a959fb42e874407ba5464669c1627ccc
Piyush <piyush.rp22@gmail.com>
2026-03-01 18:09:42 -0800

    Initial Commit

---
+++ heartbeats Watch App/heartbeatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  heartbeatsApp.swift
+//  heartbeats Watch App
+//
+//  Created by Piyush on 2026-03-01.
+//
+
+import SwiftUI
+
+@main
+struct heartbeats_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ heartbeats/heartbeatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  heartbeatsApp.swift
+//  heartbeats
+//
+//  Created by Piyush on 2026-03-01.
+//
+
+import SwiftUI
+
+@main
+struct heartbeatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
